7b9e1b8e84fe56db7773e2ad2e7235c94fb86fe3
Roilan <roilan14@gmail.com>
2016-02-13 23:52:10 -0500

    initial commit -- webpack, babel, es6, react hot loader, redux and other fun packages

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loaders: ['react-hot', 'babel'],
+      include: __dirname + '/src'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};
